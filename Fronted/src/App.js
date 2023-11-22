fd517929f0e0c9bd526bdaf527afd27ac1e39e66
satyam kumar <satyam.masai123@gmail.com>
2023-11-22 20:39:43 +0530

    9xzoom

---
+++ Fronted/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar1 from './components/main/Navbar1';
+import Home from './components/main/Home';
+
+function App() {
+  return (
+    <div className="App">
+
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
